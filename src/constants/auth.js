53569271e95b1d7116febc286945645c13e27dcc
THANH NHAN HUYNH <nhankg2016@gmail.com>
2021-10-05 22:52:34 +0700

    new change

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -1,3 +1,3 @@
-export const api_URL = 'https://morning-escarpment-37129.herokuapp.com/api'
+export const api_URL = 'https://safe-lake-14293.herokuapp.com/api'
 export const LOCAL_STORAGE_TOKEN ='setsomething'
 export const SET_AUTH = 'SET_AUTH'
