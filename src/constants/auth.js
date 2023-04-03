f83b583d2147a13e6924d66181f54fdb44f4e147
Huynh Thanh Nhan <nhan.huynh@gcalls.co>
2023-04-03 23:49:04 +0700

    update code for vercel

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -1,3 +1,3 @@
-export const api_URL = 'http://localhost:7878/api'
+export const api_URL = 'https://vercel-rust-ten.vercel.app/api'
 export const LOCAL_STORAGE_TOKEN = 'setsomething'
 export const SET_AUTH = 'SET_AUTH'
